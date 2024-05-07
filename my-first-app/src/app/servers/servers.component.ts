4bb2b5f4b9144cb7a000d90c8a5756df4060492e
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-05-07 15:10:59 -0500

    Feat: Added examples of selector by attribute and class

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //also posible to define inline templates
+  //it is also possible to select by attribute
+  //selector: '[app-server]'
+  //or by class
+  //selector: '.app-server'
+  //also possible to define inline templates
   template: `
   <app-server></app-server>
   <app-server></app-server>`,
