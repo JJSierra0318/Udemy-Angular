497f52cfbb6d4b3a90dce54d435ccaa108f8883b
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-05-07 10:55:15 -0500

    Feat: modifying app component and adding an input

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import FormsModule to use ngModel
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //add FormsModule to app module
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
