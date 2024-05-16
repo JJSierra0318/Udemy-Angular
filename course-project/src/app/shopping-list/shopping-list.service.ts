e40f40771a437ec3b6ad8720659109f21ad864f8
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-05-16 16:11:24 -0500

    Feat: refactor shopping list to use services

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,22 @@
-import { Injectable } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    // an easy alternative wold be to remove .slice from service so the actual array is passed
+    // this solution keeps sending the copy but informs with an event of the updates made
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
