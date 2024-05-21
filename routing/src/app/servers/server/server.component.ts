8cd82138eb7c909f0a464864ed399eac032bb49a
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-05-21 16:09:17 -0500

    Feat: nested/child routes

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -17,8 +17,11 @@ export class ServerComponent implements OnInit {
     // + transform id from string to number
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    
     this.route.params.subscribe(
-      (params: Params) => this.serversService.getServer(+params['id'])
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id'])
+      }
     )
   }
 
