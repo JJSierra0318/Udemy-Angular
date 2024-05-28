f269c79f6c85f31d974f4a19d3b02b7afbc8bc69
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-05-28 15:18:47 -0500

    Feat: using operators to modify data form an observable

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription, interval } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -30,7 +31,16 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscritpion = customIntervalObservable.subscribe(data => {
+    //operator to modify data from an observable
+    /* customIntervalObservable.pipe(map((data: number) => {
+      return 'Round ' + (data + 1);
+    })) */
+
+    this.firstObsSubscritpion = customIntervalObservable.pipe(filter((data: number) => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
