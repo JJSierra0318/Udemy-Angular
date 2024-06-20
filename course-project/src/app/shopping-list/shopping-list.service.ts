b41a2a63ebea805bee66402197d2ed7a196b6708
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-06-20 09:51:59 -0500

    Feat: clear and delete buttons for shopping list form

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,7 @@ import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { Subject } from "rxjs";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
   startedEditing = new Subject<number>();
@@ -33,6 +33,11 @@ export class ShoppingListService {
 
   updatedIngredient(index: number, newIngredient: Ingredient) {
     this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
